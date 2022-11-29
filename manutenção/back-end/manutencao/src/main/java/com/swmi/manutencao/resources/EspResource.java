adfa690a63d1153aa48182789670028f456fd344
Rodrigues Lucas (M) <f75329a@marelliad.net>
2022-11-29 17:17:16 -0300

    mudança para esp32

--- manutenção/back-end/manutencao/src/main/java/com/swmi/manutencao/resources/EspResource.java
+++ manutenção/back-end/manutencao/src/main/java/com/swmi/manutencao/resources/EspResource.java
@@ -12,7 +12,7 @@ public class EspResource {
     @Autowired
     private EspService espService;
 
-    @PostMapping("/gravar/{equip}/{parametro}/{valor}")
+    @GetMapping("/gravar/{equip}/{parametro}/{valor}")
     public Esp gravar(@PathVariable String equip, @PathVariable String parametro, @PathVariable String valor){
         return espService.Salvar(equip, parametro, valor);
     }
